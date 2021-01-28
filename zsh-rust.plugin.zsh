997dea34bdb40df0390a648c25ea29f96ab44120
Charles Lowell <cowboyd@frontside.io>
2021-01-27 19:10:52 -0600

    Enhance the path. Don't clobber the path

--- zsh-rust.plugin.zsh
+++ zsh-rust.plugin.zsh
@@ -1,7 +1,7 @@
 # Setup rustup and cargo environments
 export RUSTUP_HOME=$HOME/.rustup
 export CARGO_HOME=$HOME/.cargo
-export PATH=$CARGO_HOME/bin
+export PATH=$CARGO_HOME/bin:$PATH
 
 # Install rustup if it isn't installed already
 if ! [[ -s "${HOME}/.rustup" ]]; then
