e04aec0ff3be8b5e98c9ce0338796a01c099b3fe
Collin Williams <96917990+bluedragon1221@users.noreply.github.com>
2024-03-21 08:06:29 -0500

    make rust directories not pollute $HOME

--- zsh-rust.plugin.zsh
+++ zsh-rust.plugin.zsh
@@ -1,9 +1,9 @@
-# Setup rustup and cargo environments
-export RUSTUP_HOME=$HOME/.rustup
-export CARGO_HOME=$HOME/.cargo
-export PATH=$CARGO_HOME/bin:$PATH
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+
+export PATH="${PATH}:${CARGO_HOME}/bin:${RUSTUP_HOME}/toolchains/stable-x86_64-unknown-linux-gnu/bin"
 
 # Install rustup if it isn't installed already
-if ! [[ -s "${HOME}/.rustup" ]]; then
+if ! [[ -d "${RUSTUP_HOME}" ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- --no-modify-path -y
 fi
